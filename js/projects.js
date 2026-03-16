c8b33a3bea27a43775ed8126753db5228bd7a5c5
William Wadde <williamwadde@gmail.com>
2026-03-16 01:28:51 -0500

    Agregar proyecto de agencia

--- js/projects.js
+++ js/projects.js
@@ -31,6 +31,25 @@ const projects = [
         ],
     },
     {
+        title: { 
+        es: "Plataforma de Reservas para Agencia de Viajes",
+        en: "Travel Agency Booking Platform" },
+        description: { 
+            es: "Plataforma web donde los usuarios pueden explorar paquetes turísticos, revisar fechas de salida, reservar viajes y completar pagos en línea. Incluye autenticación, gestión de perfil y seguimiento de reservas. Además, cuenta con un panel administrativo para gestionar todo el contenido de la agencia dinámicamente en tiempo real, centralizando toda la operación comercial de la agencia en una sola aplicación.",
+            en: "Web platform where users can browse travel packages, check departure dates, book trips, and complete online payments. It includes authentication, profile management, and reservation tracking. It also provides an admin panel to manage the entire content of the agency dynamically in real-time, centralizing the agency’s full commercial operation in a single application." },
+        technologies: ["Angular 21", "TypeScript", "Tailwind CSS", "Signals", "REST APIs", "Role-based Authentication", "Online Payments (PayU)", "Nginx", "Docker", "GitHub Actions", "AWS EC2", "SpringBoot 4"],
+        link: "https://agencia.williamwadde.work",
+        images: [
+            "/assets/projects/agencia/landing.webp",
+            "/assets/projects/agencia/panel-administrador.webp",
+            "/assets/projects/agencia/pasarela-pago.webp",
+            "/assets/projects/agencia/transaccion-aprobada.webp",
+            "/assets/projects/agencia/paquete.webp",
+            "/assets/projects/agencia/reservas.webp",
+            "/assets/projects/agencia/subida-imagenes.webp"
+        ]
+     },
+    {
         title: {
             es: "Sistema Bancario de Créditos",
             en: "Banking Credit System",
